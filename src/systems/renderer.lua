95cdc3b8f9b1c75195e8191729400a8976a66147
Thomas Smallridge <tom@smallridge.com.au>
2020-02-22 11:22:18 +0800

    draw current phase objects on top

--- src/systems/renderer.lua
+++ src/systems/renderer.lua
@@ -13,32 +13,55 @@ function renderer:phase_update(new_phase)
   self.current_phase = new_phase
 end
 
-function renderer:draw()
-  love.graphics.setLineWidth(2)
-  for i = 1, self.pool.size do
-    local e = self.pool:get(i)
-    local transform = e:get(_components.transform)
-    local position = transform.position
-    local polygon = e:get(_components.polygon)
+function renderer:draw_phased_polygon(e)
+  local transform = e:get(_components.transform)
+  local position = transform.position
+  local polygon = e:get(_components.polygon)
+  local phase = e:get(_components.phase)
 
-    local draw_mode = "fill"
-    local phase = e:get(_components.phase)
+  local draw_mode = "fill"
 
-    if phase then
-      if self.current_phase ~= phase.current then
-        draw_mode = "line"
-      end
-
-      love.graphics.setColor(self.colours[phase.current])
+  if phase then
+    if self.current_phase ~= phase.current then
+      draw_mode = "line"
     end
 
-    love.graphics.polygon(draw_mode, polygon:calculate_world_vertices(position))
+    love.graphics.setColor(self.colours[phase.current])
+  end
+
+  love.graphics.polygon(draw_mode, polygon:calculate_world_vertices(position))
+end
+
+function renderer:draw()
+  love.graphics.setLineWidth(2)
+
+  local current_phase_drawables = {}
+  local alternate_phase_drawables = {}
+  for i = 1, self.pool.size do
+    local e = self.pool:get(i)
+    local phase = e:get(_components.phase)
+    if phase then
+      if phase.current == self.current_phase then
+        table.insert(current_phase_drawables, e)
+      else
+        table.insert(alternate_phase_drawables, e)
+      end
+    else
+      -- draw phase-less objects
+    end
 
     _util.l.resetColour()
   end
 
+  for i, poly in ipairs(alternate_phase_drawables) do
+    self:draw_phased_polygon(poly)
+  end
+
+  for i, poly in ipairs(current_phase_drawables) do
+    self:draw_phased_polygon(poly)
+  end
+
   love.graphics.setLineWidth(1)
-  -- TODO: use multiple pools to draw the player manually last (on top of shapes)
 end
 
 return renderer
